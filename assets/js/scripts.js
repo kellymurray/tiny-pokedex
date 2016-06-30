f669c062fb87b2a074eebe6d45ca37c3115f7dfa
Kelly Murray <kelly@theironyard.com>
2016-06-29 21:32:33 -0400

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Type in the name of a Pokemon',
+    query: '',
+  },
+  methods: {
+    getPokemon: function(query){
+      console.log(this.message);
+      $.ajax({
+        dataType: 'json',
+        url: 'http://pokeapi.co/api/v2/pokemon/' + query,
+        method: 'GET',
+      }).done(function(data) {
+        console.log(data);
+      })
+    }
+  }
+});
+
+// TESTING WITH AJAX BELOW
+//
+// var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "http://pokeapi.co/api/v2/pokedex/",
+//   "method": "GET",
+//   "headers": {
+//     "cache-control": "no-cache",
+//     "postman-token": "cf30349e-20af-9239-3830-c910aee70506"
+//   }
+// }
+//
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
