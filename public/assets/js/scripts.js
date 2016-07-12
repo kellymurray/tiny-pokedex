93366210a364255fcb6c9cc2d50b1edc8fd9a46f
Tori Hedden <tori.hedden@gmail.com>
2016-07-12 13:04:40 -0400

    file re-structure for firebase deploy

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+//API call + toLowerCase function
+var pokeApp = new Vue({
+  el: '#pokeApp',
+  data: {
+    message: 'Gotta catch who?',
+    query: '',
+    errorMessage: "Oops! A wild Pokemon didn't appear."
+  },
+  methods: {
+    getPokemon: function(query){
+      var pokeQuery = query.toLowerCase();
+      $(".loadingIcon").show();
+      $.ajax({
+        dataType: 'json',
+        url: 'https://pokeapi.co/api/v2/pokemon/' + pokeQuery,
+        method: 'GET',
+        success: function(data, textStatus ){
+          pokeApp.$set("name", data.name);
+          pokeApp.$set("sprite", data.sprites.front_default);
+        },
+        error: function(data, textStatus){
+          $(".errorMessage").show();
+          $(".loadingIcon").hide();
+        }
+      }).done(function(data) {
+        $(".loadingIcon").hide();
+      })
+    },
+    clearQuery: function(){
+      pokeApp.$set("name", "");
+      pokeApp.$set("sprite", "");
+      pokeApp.$set("query", "");
+      $('.poke-choice').hide();
+      $('.poke-search').show();
+    }
+  }
+});
+
+$('.poke-button').on('click', function() {
+  $('.poke-choice').removeClass("hide").addClass("show").fadeIn('fast');
+});
+
+$('.poke-button').on('click', function () {
+  $('.poke-search').fadeOut('fast');
+});
+
+
+
+}); //end
